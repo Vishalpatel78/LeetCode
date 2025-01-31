0bbcfdbf8ccc50f7b467ed193d342f17dbf3c6e0
Vishal Kurmi <114057992+Vishalpatel78@users.noreply.github.com>
2025-01-31 15:25:53 +0530

    Time: 0 ms (100%), Space: 63 MB (32.09%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-    int maxWater = 0; // To store the maximum area
-    int left = 0, right = height.size() - 1;
-
-    while (left < right) {
-        // Calculate the area between the two pointers
-        int currentArea = min(height[left], height[right]) * (right - left);
-        maxWater = max(maxWater, currentArea);
-
-        // Move the pointer pointing to the shorter line
-        if (height[left] < height[right]) {
-            ++left;
-        } else {
-            --right;
+    int maxArea(vector<int>& arr) {
+        int n = arr.size();
+        int j= n-1;
+        int st = 0;
+        int water = 0;
+        for(int i=0; i<n; i++){
+            int width = j - st;
+            int height = min (arr[st], arr[j]);
+            int curntWater = width * height;
+            water = max(curntWater,water);
+            if(arr[st]<arr[j]){
+                st++;
+            }
+            else{
+                j--;
+            }
         }
+        return water;
     }
-
-    return maxWater;
-}
 };
